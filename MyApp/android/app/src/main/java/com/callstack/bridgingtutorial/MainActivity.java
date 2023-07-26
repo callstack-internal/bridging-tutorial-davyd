371b093f9286a177f8a0e82f1c9d85eac1887628
D N <4661784+retyui@users.noreply.github.com>
2023-07-26 13:40:26 +0200

    npx react-native-rename

--- MyApp/android/app/src/main/java/com/callstack/bridgingtutorial/MainActivity.java
+++ MyApp/android/app/src/main/java/com/callstack/bridgingtutorial/MainActivity.java
@@ -1,4 +1,4 @@
-package com.myapp;
+package com.callstack.bridgingtutorial;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- MyApp/android/app/src/main/java/com/callstack/bridgingtutorial/MainApplication.java
+++ MyApp/android/app/src/main/java/com/callstack/bridgingtutorial/MainApplication.java
@@ -1,4 +1,4 @@
-package com.myapp;
+package com.callstack.bridgingtutorial;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
